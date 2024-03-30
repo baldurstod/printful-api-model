58846214bf27c4ad8a2ef379b48110a4e68f75b6
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-03-30 23:57:48 +0100

    Added bson tags

---
+++ product.go
@@ -0,0 +1,21 @@
+package model
+
+type Product struct {
+	ID                 int                  `json:"id" bson:"id"`
+	MainCategoryID     int                  `json:"main_category_id" bson:"main_category_id"`
+	Type               string               `json:"type" bson:"type"`
+	TypeName           string               `json:"type_name" bson:"type_name"`
+	Title              string               `json:"title" bson:"title"`
+	Brand              string               `json:"brand" bson:"brand"`
+	Model              string               `json:"model" bson:"model"`
+	Image              string               `json:"image" bson:"image"`
+	VariantCount       int                  `json:"variant_count" bson:"variant_count"`
+	Currency           string               `json:"currency" bson:"currency"`
+	Files              []FileType           `json:"files" bson:"files"`
+	Options            []OptionType         `json:"options" bson:"options"`
+	IsDiscontinued     bool                 `json:"is_discontinued" bson:"is_discontinued"`
+	AvgFulfillmentTime float64              `json:"avg_fulfillment_time" bson:"avg_fulfillment_time"`
+	Description        string               `json:"description" bson:"description"`
+	Techniques         []AvailableTechnique `json:"techniques" bson:"techniques"`
+	OriginCountry      string               `json:"origin_country" bson:"origin_country"`
+}
--- productresponse.go
+++ productresponse.go
@@ -6,23 +6,3 @@ type ProductResponse struct {
 		Product `json:"pf"`
 	} `json:"result"`
 }
-
-type Product struct {
-	ID                 int                  `json:"id"`
-	MainCategoryID     int                  `json:"main_category_id"`
-	Type               string               `json:"type"`
-	TypeName           string               `json:"type_name"`
-	Title              string               `json:"title"`
-	Brand              string               `json:"brand"`
-	Model              string               `json:"model"`
-	Image              string               `json:"image"`
-	VariantCount       int                  `json:"variant_count"`
-	Currency           string               `json:"currency"`
-	Files              []FileType           `json:"files"`
-	Options            []OptionType         `json:"options"`
-	IsDiscontinued     bool                 `json:"is_discontinued"`
-	AvgFulfillmentTime float64              `json:"avg_fulfillment_time"`
-	Description        string               `json:"description"`
-	Techniques         []AvailableTechnique `json:"techniques"`
-	OriginCountry      string               `json:"origin_country"`
-}
