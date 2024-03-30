26e96f6280be1c1c096838a033dba7aed2bdd9a8
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-03-30 13:42:49 +0100

    Fixed 2 properties type

--- optiontype.go
+++ optiontype.go
@@ -1,10 +1,10 @@
 package model
 
 type OptionType struct {
-	ID                       string            `json:"id"`
-	Title                    string            `json:"title"`
-	Type                     string            `json:"type"`
-	Values                   map[string]string `json:"values"`
-	AdditionalPrice          string            `json:"additional_price"`
-	AdditionalPriceBreakdown map[string]string `json:"additional_price_breakdown"`
+	ID                       string      `json:"id"`
+	Title                    string      `json:"title"`
+	Type                     string      `json:"type"`
+	Values                   interface{} `json:"values"`
+	AdditionalPrice          string      `json:"additional_price"`
+	AdditionalPriceBreakdown interface{} `json:"additional_price_breakdown"`
 }
