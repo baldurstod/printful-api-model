b40f64f5faec5aae1a15e2a6b788c264e33e77e2
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-04-04 00:29:02 +0200

    Added GetPrintfile()

--- printfileinfo.go
+++ printfileinfo.go
@@ -8,3 +8,23 @@ type PrintfileInfo struct {
 	OptionGroups        []string           `json:"option_groups" bson:"option_groups"`
 	Options             []string           `json:"options" bson:"options"`
 }
+
+func (printfileInfo PrintfileInfo) GetPrintfile(variantID int, placement string) *Printfile {
+	for _, v := range printfileInfo.VariantPrintfiles {
+		if v.VariantID == variantID {
+
+			printfileID, ok := v.Placements.(map[string]interface{})[placement]
+			if !ok {
+				return nil
+			}
+
+			for _, p := range printfileInfo.Printfiles {
+				if p.PrintfileID == int(printfileID.(float64)) {
+					return &p
+				}
+			}
+			return nil
+		}
+	}
+	return nil
+}
