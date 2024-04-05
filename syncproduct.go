5d3cf00860b50a261200d34cd07725f07f4ea19a
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-04-05 14:34:24 +0200

    Added SyncProduct

---
+++ syncproduct.go
@@ -0,0 +1,12 @@
+package model
+
+type SyncProduct struct {
+	ID           int    `json:"id" bson:"id"`
+	ExternalID   string `json:"external_id" bson:"external_id"`
+	Name         string `json:"name" bson:"name"`
+	Variants     int    `json:"variants" bson:"variants"`
+	Synced       int    `json:"synced" bson:"synced"`
+	Thumbnail    string `json:"thumbnail" bson:"thumbnail"`
+	ThumbnailURL string `json:"thumbnail_url" bson:"thumbnail_url"`
+	IsIgnored    bool   `json:"is_ignored" bson:"is_ignored"`
+}
