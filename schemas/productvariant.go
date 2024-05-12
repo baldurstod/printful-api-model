db2ec85dc773774469eba781204e879c99330488
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-05-12 19:31:36 +0200

    Added mapstructure

--- schemas/productvariant.go
+++ schemas/productvariant.go
@@ -1,8 +1,8 @@
-package model
+package schemas
 
 type ProductVariant struct {
-	VariantID int    `json:"variant_id" bson:"variant_id"`
-	ProductID int    `json:"product_id" bson:"product_id"`
-	Image     string `json:"image" bson:"image"`
-	Name      string `json:"name" bson:"name"`
+	VariantID int    `json:"variant_id" bson:"variant_id" mapstructure:"variant_id"`
+	ProductID int    `json:"product_id" bson:"product_id" mapstructure:"product_id"`
+	Image     string `json:"image" bson:"image" mapstructure:"image"`
+	Name      string `json:"name" bson:"name" mapstructure:"name"`
 }
