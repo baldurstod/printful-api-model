8d01795bd82cf50b11ce4a89f4089af3c2963af7
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-05-21 09:11:22 +0200

    Added omitempty tags

--- schemas/item.go
+++ schemas/item.go
@@ -1,21 +1,21 @@
 package schemas
 
 type Item struct {
-	ID                        int64          `json:"id" bson:"id" mapstructure:"id"`
-	ExternalID                string         `json:"external_id" bson:"external_id" mapstructure:"external_id"`
-	VariantID                 int64          `json:"variant_id" bson:"variant_id" mapstructure:"variant_id"`
-	SyncVariantID             int64          `json:"sync_variant_id" bson:"sync_variant_id" mapstructure:"sync_variant_id"`
-	ExternalVariantID         string         `json:"external_variant_id" bson:"external_variant_id" mapstructure:"external_variant_id"`
-	WarehouseProductVariantID int64          `json:"warehouse_product_variant_id" bson:"warehouse_product_variant_id" mapstructure:"warehouse_product_variant_id"`
-	ProductTemplateID         int64          `json:"product_template_id" bson:"product_template_id" mapstructure:"product_template_id"`
-	ExternalProductID         string         `json:"external_product_id" bson:"external_product_id" mapstructure:"external_product_id"`
+	ID                        int64          `json:"id",omitempty bson:"id" mapstructure:"id"`
+	ExternalID                string         `json:"external_id",omitempty bson:"external_id" mapstructure:"external_id"`
+	VariantID                 int64          `json:"variant_id",omitempty bson:"variant_id" mapstructure:"variant_id"`
+	SyncVariantID             int64          `json:"sync_variant_id",omitempty bson:"sync_variant_id" mapstructure:"sync_variant_id"`
+	ExternalVariantID         string         `json:"external_variant_id",omitempty bson:"external_variant_id" mapstructure:"external_variant_id"`
+	WarehouseProductVariantID int64          `json:"warehouse_product_variant_id",omitempty bson:"warehouse_product_variant_id" mapstructure:"warehouse_product_variant_id"`
+	ProductTemplateID         int64          `json:"product_template_id",omitempty bson:"product_template_id" mapstructure:"product_template_id"`
+	ExternalProductID         string         `json:"external_product_id",omitempty bson:"external_product_id" mapstructure:"external_product_id"`
 	Quantity                  int            `json:"quantity" bson:"quantity" mapstructure:"quantity"`
 	Price                     string         `json:"price" bson:"price" mapstructure:"price"`
 	RetailPrice               string         `json:"retail_price" bson:"retail_price" mapstructure:"retail_price"`
 	Name                      string         `json:"name" bson:"name" mapstructure:"name"`
 	Product                   ProductVariant `json:"product" bson:"product" mapstructure:"product"`
-	Files                     []File         `json:"files" bson:"files" mapstructure:"files"`
-	Options                   []Option       `json:"options" bson:"options" mapstructure:"options"`
+	Files                     []File         `json:"files",omitempty bson:"files" mapstructure:"files"`
+	Options                   []Option       `json:"options",omitempty bson:"options" mapstructure:"options"`
 	SKU                       string         `json:"sku" bson:"sku" mapstructure:"sku"`
 	Discontinued              bool           `json:"discontinued" bson:"discontinued" mapstructure:"discontinued"`
 	OutOfStock                bool           `json:"out_of_stock" bson:"out_of_stock" mapstructure:"out_of_stock"`
