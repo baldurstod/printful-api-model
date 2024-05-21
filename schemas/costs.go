37c5e052dfe1ba66b00dafc221fe9397bd599349
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-05-21 10:23:27 +0200

    Fixed data types

--- schemas/costs.go
+++ schemas/costs.go
@@ -1,15 +1,15 @@
 package schemas
 
 type Costs struct {
-	Currency          string `json:"currency" bson:"currency" mapstructure:"currency"`
-	Subtotal          string `json:"subtotal" bson:"subtotal" mapstructure:"subtotal"`
-	Discount          string `json:"discount" bson:"discount" mapstructure:"discount"`
-	Shipping          string `json:"shipping" bson:"shipping" mapstructure:"shipping"`
-	Digitization      string `json:"digitization" bson:"digitization" mapstructure:"digitization"`
-	AdditionalFee     string `json:"additional_fee" bson:"additional_fee" mapstructure:"additional_fee"`
-	FulfillmentFee    string `json:"fulfillment_fee" bson:"fulfillment_fee" mapstructure:"fulfillment_fee"`
-	RetailDeliveryFee string `json:"retail_delivery_fee" bson:"retail_delivery_fee" mapstructure:"retail_delivery_fee"`
-	Tax               string `json:"tax" bson:"tax" mapstructure:"tax"`
-	Vat               string `json:"vat" bson:"vat" mapstructure:"vat"`
-	Total             string `json:"total" bson:"total" mapstructure:"total"`
+	Currency          string  `json:"currency" bson:"currency" mapstructure:"currency"`
+	Subtotal          float64 `json:"subtotal" bson:"subtotal" mapstructure:"subtotal"`
+	Discount          float64 `json:"discount" bson:"discount" mapstructure:"discount"`
+	Shipping          float64 `json:"shipping" bson:"shipping" mapstructure:"shipping"`
+	Digitization      float64 `json:"digitization" bson:"digitization" mapstructure:"digitization"`
+	AdditionalFee     float64 `json:"additional_fee" bson:"additional_fee" mapstructure:"additional_fee"`
+	FulfillmentFee    float64 `json:"fulfillment_fee" bson:"fulfillment_fee" mapstructure:"fulfillment_fee"`
+	RetailDeliveryFee string  `json:"retail_delivery_fee" bson:"retail_delivery_fee" mapstructure:"retail_delivery_fee"`
+	Tax               string  `json:"tax" bson:"tax" mapstructure:"tax"`
+	Vat               float64 `json:"vat" bson:"vat" mapstructure:"vat"`
+	Total             float64 `json:"total" bson:"total" mapstructure:"total"`
 }
