ddb2c158318c08d4ccabe534bc8ff459611b2569
Guillaume <108530065+baldurstod@users.noreply.github.com>
2024-05-21 10:31:06 +0200

    Fixed data types

--- schemas/retailcosts.go
+++ schemas/retailcosts.go
@@ -1,11 +1,11 @@
 package schemas
 
 type RetailCosts struct {
-	Currency string `json:"currency" bson:"currency" mapstructure:"currency"`
-	Subtotal string `json:"subtotal" bson:"subtotal" mapstructure:"subtotal"`
-	Discount string `json:"discount" bson:"discount" mapstructure:"discount"`
-	Shipping string `json:"shipping" bson:"shipping" mapstructure:"shipping"`
-	Tax      string `json:"tax" bson:"tax" mapstructure:"tax"`
-	Vat      string `json:"vat" bson:"vat" mapstructure:"vat"`
-	Total    string `json:"total" bson:"total" mapstructure:"total"`
+	Currency string  `json:"currency" bson:"currency" mapstructure:"currency"`
+	Subtotal float64 `json:"subtotal" bson:"subtotal" mapstructure:"subtotal"`
+	Discount float64 `json:"discount" bson:"discount" mapstructure:"discount"`
+	Shipping float64 `json:"shipping" bson:"shipping" mapstructure:"shipping"`
+	Tax      float64 `json:"tax" bson:"tax" mapstructure:"tax"`
+	Vat      float64 `json:"vat" bson:"vat" mapstructure:"vat"`
+	Total    float64 `json:"total" bson:"total" mapstructure:"total"`
 }
